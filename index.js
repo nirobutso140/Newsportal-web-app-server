0700286553c37276c09f867993544452406d5c61
nirobutso140 <nirobutso140@gmail.com>
2023-11-28 01:39:33 +0600

    create get api to get users data

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
