c3d366a13a5a5cdef1ae893b06388a0834465f19
nirobutso140 <nirobutso140@gmail.com>
2023-11-28 15:58:46 +0600

    create get api to load user, and create patch api to update user as admin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iabkpld.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,6 +40,21 @@ async function run() {
 
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedUSer = {
+        $set: {
+          role: "admin"
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedUSer,
+      );
       res.send(result);
     });
 
