2efbf2b1ce868d3574aecbecd288d604d1ca7a4b
nirobutso140 <nirobutso140@gmail.com>
2023-12-03 23:20:03 +0600

    to display article details info, givewebsite name and favicon, and responsive of home page

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/article/:id", async (req, res) => {
+    app.get("/articleDetails/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id),
