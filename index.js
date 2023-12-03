606503a7f6a4dfebfb4d50334430d88ae762a839
nirobutso140 <nirobutso140@gmail.com>
2023-12-03 15:18:22 +0600

    create get api to get single article data

--- index.js
+++ index.js
@@ -75,6 +75,16 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/article/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await artileCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
     app.patch("/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
